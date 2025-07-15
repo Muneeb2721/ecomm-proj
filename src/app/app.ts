877f1eb7cd3e69dd7818a0e7edeebd12c740dc36
Muneeb2721 <muneebnadeem1870@gmail.com>
2025-07-15 17:46:10 +0500

    Routing setting of 2 components Home and Seller

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,7 +4,7 @@ import { Header } from "./header/header";
 
 @Component({
   selector: 'app-root',
-  imports: [Header],
+  imports: [Header, RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
