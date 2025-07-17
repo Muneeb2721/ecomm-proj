f622ca13810ae1460e24ff9c77de0ead44b7aec1
Muneeb2721 <muneebnadeem1870@gmail.com>
2025-07-17 16:33:53 +0500

    correct format code

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,8 @@ import { Home } from './home/home';
 import { SellerAuth } from './seller-auth/seller-auth';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClient, provideHttpClient } from '@angular/common/http';
+
 
 export const routes: Routes = [
     {
@@ -21,8 +22,9 @@ export const routes: Routes = [
         RouterModule.forRoot(routes),
         FormsModule,
         Home,
-        HttpClientModule
+        
     ],
     exports: [RouterModule]
 })
-export class AppRoutingModule { }
+
+export class AppRoutingModule {}
