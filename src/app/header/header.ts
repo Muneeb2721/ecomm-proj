0c7f62e3c3cd2809b0523032cdabc1190d571aa1
Muneeb2721 <muneebnadeem1870@gmail.com>
2025-07-23 12:03:45 +0500

    toggling menus on different mode implement

--- src/app/header/header.ts
+++ src/app/header/header.ts
@@ -1,13 +1,40 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { RouterModule, Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [RouterModule, CommonModule],
+  standalone: true,
+  imports: [RouterModule, CommonModule], // ✅ This is correct for standalone components
   templateUrl: './header.html',
   styleUrl: './header.css'
 })
-export class Header {
+export class Header implements OnInit {
+  menuType: string = 'default';
+  sellerName: string = '';
 
+  constructor(private route: Router) {}
+
+  ngOnInit(): void {
+    this.route.events.subscribe((val: any) => {
+      if (val.url) {
+        if (localStorage.getItem('seller') && val.url.includes('seller')) {
+          this.menuType = 'seller';
+          if (localStorage.getItem('seller')) {
+            let sellerStore = localStorage.getItem('seller');
+            let sellerData = sellerStore && JSON.parse(sellerStore)[0];
+            this.sellerName = sellerData.name;
+          }
+        } else {
+          this.menuType = 'default';
+        }
+      }
+    });
+  }
+
+  logout() {
+    localStorage.removeItem('seller');
+    this.route.navigate(['/']);
+    this.menuType = 'default';
+  }
 }
